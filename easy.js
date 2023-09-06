0c6dd5a4a74edd93107e54084f7933d4d8299bba
ChevvyChelios <souravsarania2@gmail.com>
2023-09-06 14:52:39 +0530

    edited

--- easy.js
+++ easy.js
@@ -1,5 +1,4 @@
 
-// let body = document.getElementById('body');
 
 function changeColor(){
     var c1 = Math.floor(Math.random() * 256);
@@ -11,5 +10,4 @@ function changeColor(){
     var de = Math.floor(Math.random() * 361);
     console.log(c1,c2,c3);
     document.body.style.background = 'linear-gradient('+de+'deg, rgb('+c1+',' +c2+', '+c3+'),'+'rgb('+c11+',' +c12+', '+c13+'))';
-    // body.style.backgroundColor= 'rgb('+c1+',' +c2+', '+c3+')';
 }
