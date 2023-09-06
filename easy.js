2bb9063068fd8e615b4cd522bb4445d465b46c60
ChevvyChelios <souravsarania2@gmail.com>
2023-09-06 14:51:31 +0530

    changed a little

--- easy.js
+++ easy.js
@@ -1,5 +1,5 @@
 
-let body = document.getElementById('body');
+// let body = document.getElementById('body');
 
 function changeColor(){
     var c1 = Math.floor(Math.random() * 256);
@@ -10,6 +10,6 @@ function changeColor(){
     var c13 = Math.floor(Math.random() * 256);
     var de = Math.floor(Math.random() * 361);
     console.log(c1,c2,c3);
-    body.style.background = 'linear-gradient('+de+'deg, rgb('+c1+',' +c2+', '+c3+'),'+'rgb('+c11+',' +c12+', '+c13+'))';
+    document.body.style.background = 'linear-gradient('+de+'deg, rgb('+c1+',' +c2+', '+c3+'),'+'rgb('+c11+',' +c12+', '+c13+'))';
     // body.style.backgroundColor= 'rgb('+c1+',' +c2+', '+c3+')';
 }
